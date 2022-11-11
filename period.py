3aae36b0638c3de97bcba14f6bc509bce075b035
MrCamoga <mrcamoga@gmail.com>
2022-11-11 17:27:18 +0100

    simplified algorithm

--- period.py
+++ period.py
@@ -17,31 +17,29 @@ class PeriodicList:
 		return x
 
 def computePeriod(l):
-	match=1
+	match=0
 	period=0
 	states=[0]*STATESSIZE
-	buffer=[]
 	si=0
 	for s in l:
-		if si < len(states):
-			states[si] = s
-		si += 1
 		if states[match]!=s:
-			match=0
-			period+=1
-			while len(buffer)>0:
-				for i in range(len(buffer)):
-					if states[match]!=buffer[i]:
-						match=0
+			m=0
+			period=si+1
+			while match>0:
+				for i in range(match):
+					if states[m]!=states[m+i]:
+						m=0
 						break
 					else:
-						match+=1
+						m+=1
 				else:
-					break # ESTO FALTA EN EL DE JAVA
-				buffer.remove(0)
-				period+=1
+					break
+				match-=1
+			period-=match
 		else:
 			match += 1
-			buffer.append(s)
 			if match > MINMATCH and (match == len(states) or match > REPEATPERCENT*period):
 				return period,states[0:period]
+		if si < len(states):
+			states[si] = s
+		si+=1
--- tests.py
+++ tests.py
@@ -1,5 +1,13 @@
 from period import *
+from random import randint
 
 sequences = [[0,1,2,3,4,5,0,1,2,3],[3,5,7],[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9],[0, 0, 0, 1, 1],[2,5,1,2,5,1,2,3],[1,2],[3],[1,2,1]]
 for s in sequences:
-        print(computePeriod(PeriodicList(s)))
+	print(computePeriod(PeriodicList(s)))
+
+for _ in range(10):
+	length = randint(10,100000)
+	s = [randint(0,255) for _ in range(length)]
+	p,l=computePeriod(PeriodicList(s))
+	if p!=length:
+		print(p)
